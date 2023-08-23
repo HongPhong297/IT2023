2de20349d840887c63becc0f5840d3987fb301da
HongPhong297 <roxphong@gmail.com>
2023-08-23 22:10:15 +0700

    update23.08.2023

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -24,21 +24,99 @@ void insertFirst(Node *&pHead, int data){
     p->nextNode = pHead;
     pHead = p;
 }
-int main(){
+void insertLast(Node *&pHead, int data){
+    if(isEmpty(pHead) == 1)
+        insertFirst(pHead, data);
+    else{
+        Node *z = createNode(data);
+        Node *k;
+        for(k = pHead; k->nextNode != NULL; k = k->nextNode){
+
+        }
+        k->nextNode = z;
+
+    }
+        
+}
+void insertAfter(Node *pHead, int data, int index){
+    // giả sử đã có 1 linked lish sẵn 
+    Node *k = pHead;
+    //for(k = pHead; k->nextNode != NULL; k = k->nextNode)
+    while(k->nextNode != NULL){
+        k = k->nextNode;
+    }
+    if(k->data == index)
+        insertLast(pHead,data);
+    else{
+        k = pHead;
+        while(k->nextNode != NULL){
+            if(k->data == index){
+                Node *p = createNode(data);
+                Node *x = k->nextNode;
+                k->nextNode = p;
+                p->nextNode = x;
+            }
+            k = k->nextNode;
+        }
+
+    }
+
+}
+void deleteFirstNode(Node *&pHead){
+    Node *temp = pHead;
+    pHead = pHead->nextNode;
+    delete temp;
+
+}
+void deleteAfterNode(Node *pHead, int index){
+    for(Node *k = pHead; k->nextNode != NULL; k = k->nextNode){
+        if(k->data == index){
+            Node *p = k->nextNode;
+            k->nextNode = p->nextNode;
+            delete p; 
+        }
+    }
+
+}
+void deleteList(Node *&pHead) {
+    while (pHead != NULL) {
+        Node* temp = pHead;
+        pHead = pHead->nextNode;
+        delete temp;
+    }
+}
+void inputData(Node *&pHead){
     int volum;
     printf("Nhap so luong Node: ");
     scanf("%d",&volum);
-    Node *pHead;
-    Init(pHead);
     for(int i = 1 ; i <= volum; i++){
         int x;
         printf("Data Node %d: ",i);
         scanf("%d",&x);
         insertFirst(pHead,x);
     }
-    
+}
+void showData(Node *pHead){
     for(Node *i = pHead; i != NULL; i = i->nextNode){
         printf("%d ",i->data);
     }
+}
+void checkDelete(Node *pHead){
+    if(pHead == NULL)
+        printf("\nDa xoa thanh cong");
+    else
+        printf("\nErrors Detele All");
+}
+int main(){
+    Node *pHead;
+    Init(pHead);
+    inputData(pHead);
+    insertLast(pHead,10);
+    insertAfter(pHead,2907,4);
+    deleteFirstNode(pHead);
+    deleteAfterNode(pHead,6);
+    showData(pHead);
+    deleteList(pHead);
+    checkDelete(pHead);
     return 0;
 }
